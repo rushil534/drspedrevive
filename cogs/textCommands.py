240bbde7e526207e5df5568244be57b56f75ca64
rushil <64613767+rushil534@users.noreply.github.com>
2023-07-27 19:56:52 -0700

    Update textCommands.py

--- cogs/textCommands.py
+++ cogs/textCommands.py
@@ -3,8 +3,6 @@ from discord.ext import commands
 class textCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
-    
 
     @commands.command()
     async def bold(self, ctx, *, message: str):
