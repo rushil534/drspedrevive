b545ee195deb59755b4f0c4595741133501e9aa0
rushil <64613767+rushil534@users.noreply.github.com>
2023-07-29 01:30:06 -0700

    Update plead.py

--- econ/plead.py
+++ econ/plead.py
@@ -1,6 +1,5 @@
 from discord.ext import commands
 import sys
-import json
 import discord
 import random
 
@@ -8,51 +7,28 @@ sys.path.append('C:/Users/mantr/Desktop/ds-master')
 
 import bot as mainbot
 
-balances = mainbot.balances
-BALANCES_FILE = mainbot.BALANCES_FILE
-random_colors = mainbot.random_colors
-START_BAL = mainbot.START_BAL
-
-class plead(commands.Cog):
+class balance(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    @commands.cooldown(1, 60, commands.BucketType.user)
-    async def plead(self, ctx):
-        try:
-            with open(BALANCES_FILE, 'r') as fp: 
-                balances = json.load(fp) 
-        except FileNotFoundError:
-            print(f'In on_ready(): File {BALANCES_FILE} not found. Starting off with an empty balances dictionary.') 
-            balances = {}
-    
-        INCREMENT = random.randint(20, 70)
-        user = str(ctx.message.author.id) 
+    @commands.command(aliases = ['bal'])
+    async def balance(self, ctx, member: discord.Member = None): 
+        mainbot.balances = mainbot.openfile(mainbot.BALANCES_FILE)
 
-        if user in balances: 
-            names = ["**Kanye West**", "**Lindsay Lohan**", "**Azealia Banks**", "**Justin Bieber**", "**Roseanne Barr**", "**Charlie Sheen**", "**Amanda Bynes**", "**Woody Allen**", "**Mel Gibson**", "**R. Kelly**"]
-            await ctx.send(random.choice(names) + f' donated {INCREMENT} coins to {ctx.message.author.mention}!')
-            balances[user] += INCREMENT
-        else: 
-            print(f'In plead(): No record for {user} found. Creating a new record with a starting balance of {START_BAL}') 
-            balances[user] = START_BAL 
-            await ctx.send(f'hey you don\'t have a bank account yet. I just created one for you and started you off with {START_BAL} coins') 
-            self.plead.reset_cooldown(ctx)
-
-        print(f'In plead(): Saving balances = {balances}')
-        try: 
-            with open(BALANCES_FILE, 'w') as fp: 
-                json.dump(balances, fp) 
-        except FileNotFoundError: 
-            print(f'In beg(): File {BALANCES_FILE} not found! Not sure what to do here!')
-
-    @plead.error
-    async def plead_error(self, ctx, error):
-        if isinstance(error, commands.CommandOnCooldown):
-            m, s = divmod(error.retry_after, 60)
-            await ctx.send(f'wait **{round(m)} minutes and {round(s)} seconds** to plead again ')
-            return
+        user = str(ctx.message.author.id)
+        if member is None:
+            if user in mainbot.balances:
+                embed = discord.Embed(title=f":moneybag: {ctx.message.author.name}'s balance", description=f"**Bank Account**: {mainbot.balances[user]:,d}/ꝏ", color = random.choice(mainbot.random_colors))
+                await ctx.send(embed=embed)
+            else:
+                mainbot.balances[user] = 100
+                await ctx.send('You didnt have an account so i made you one with a start of 100 coins')
+        else:
+            if str(member.id) in mainbot.balances:
+                embed = discord.Embed(title=f":moneybag: {member.name}'s balance", description=f"**Bank Account**: {mainbot.balances[str(member.id)]:,d}/ꝏ", color = random.choice(mainbot.random_colors))
+                await ctx.send(embed=embed)
+            else:
+                await ctx.send('user doesn\'t have an acc')
 
 async def setup(bot):
-    await bot.add_cog(plead(bot)) 
+    await bot.add_cog(balance(bot)) 
