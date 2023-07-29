f750c432b12b3d8823a5b517b8f59d50a9926e61
rushil <64613767+rushil534@users.noreply.github.com>
2023-07-29 01:30:17 -0700

    Update rob.py

--- econ/rob.py
+++ econ/rob.py
@@ -1,16 +1,11 @@
 from discord.ext import commands
 import sys
-import json
 import discord
 import random
 
 sys.path.append('C:/Users/mantr/Desktop/ds-master')  
 
 import bot as mainbot
-
-balances = mainbot.balances
-BALANCES_FILE = mainbot.BALANCES_FILE
-START_BAL = mainbot.START_BAL
 
 class rob(commands.Cog):
     def __init__(self, bot):
@@ -19,12 +14,7 @@ class rob(commands.Cog):
     @commands.command()
     @commands.cooldown(1, 3600, commands.BucketType.user)
     async def rob(self, ctx, member: discord.Member = None):
-        try:
-            with open(BALANCES_FILE, 'r') as fp: 
-                balances = json.load(fp) 
-        except FileNotFoundError:
-            print(f'In on_ready(): File {BALANCES_FILE} not found. Starting off with an empty balances dictionary.') 
-            balances = {}
+        mainbot.balances = mainbot.openfile(mainbot.BALANCES_FILE)
 
         user = str(ctx.message.author.id)
 
@@ -35,21 +25,21 @@ class rob(commands.Cog):
             await ctx.send("?")
             self.rob.reset_cooldown(ctx)
         else:
-            if user in balances:
-                if balances[user] >= 5000:
-                    if str(member.id) in balances:
+            if user in mainbot.balances:
+                if mainbot.balances[user] >= 5000:
+                    if str(member.id) in mainbot.balances:
                         await ctx.send(f"you have a 25% chance of successfully robbing 15-30% of **{member.name}'s** account. you fail you pay 5000\ntype `y` to continue and `n` to not")
                         msg = await self.bot.wait_for('message', check = lambda m: m.author == ctx.author)
                         if msg.content.lower() == 'y':
                             CHANCE_OF_ROB = random.randint(1, 100)
                             AMOUNT_ROBBED_PERCENTAGE = random.randint(15, 30) 
                             if CHANCE_OF_ROB <= 25:
-                                amount_robbed = round((AMOUNT_ROBBED_PERCENTAGE/100) * balances[str(member.id)])
-                                balances[str(member.id)] -= amount_robbed
-                                balances[user] += amount_robbed
+                                amount_robbed = round((AMOUNT_ROBBED_PERCENTAGE/100) * mainbot.balances[str(member.id)])
+                                mainbot.balances[str(member.id)] -= amount_robbed
+                                mainbot.balances[user] += amount_robbed
                                 await ctx.send(f'nice {ctx.message.author.mention}, you robbed **{AMOUNT_ROBBED_PERCENTAGE}%** of **{member.name}\'s** bank account (${amount_robbed:,d})')
                             else:
-                                balances[user] -= 5000
+                                mainbot.balances[user] -= 5000
                                 await ctx.send('the robbery failed and you lost **$5000**')
                         elif msg.content.lower() == 'n':
                             await ctx.send('ok')
@@ -67,12 +57,7 @@ class rob(commands.Cog):
                 await ctx.send('you don\'t have a bank acc. use the `plead` command to get one')
                 self.rob.reset_cooldown(ctx)
             
-        print(f'In plead(): Saving balances = {balances}')
-        try: 
-            with open(BALANCES_FILE, 'w') as fp: 
-                json.dump(balances, fp) 
-        except FileNotFoundError: 
-            print(f'In rob(): File {BALANCES_FILE} not found! Not sure what to do here!')
+        mainbot.savefile(mainbot.balances, mainbot.BALANCES_FILE)
 
     @rob.error
     async def rob_error(self, ctx, error):
