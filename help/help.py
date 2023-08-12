05a7e629db3c482b40171c97165f2133e4a61667
rushil <64613767+rushil534@users.noreply.github.com>
2023-08-11 21:21:10 -0700

    Update help.py

--- help/help.py
+++ help/help.py
@@ -22,17 +22,13 @@ class help(commands.Cog):
 
         embed = discord.Embed(title = "Mr. Morale Command Center", description = 'cool bot', color = random.choice(mainbot.random_colors))
         
-        embed.add_field(name = ':smile: Fun', value = f'`{x}help fun`', inline = True)
+        embed.add_field(name = ':smile: Random', value = f'`{x}help random`', inline = True)
 
         embed.add_field(name = ':hammer: Moderation', value = f"`{x}help moderation`", inline = False)
 
         embed.add_field(name = ':money_with_wings: Economy', value = f"`{x}help economy`", inline = False)
 
-        embed.add_field(name = ':alien: Other', value = f"`{x}help other`", inline = False)
-
         embed.add_field(name = ':axe: Settings', value = f"`{x}help settings`", inline = False)
-
-        embed.add_field(name = ':b: Text', value = f'`{x}help text`', inline = False)
 
         embed.set_footer(text = f'use `{x}` before each command!')
 
@@ -50,15 +46,15 @@ class help(commands.Cog):
         embed.set_footer(text = f'use `{x}` before each command!')
         await ctx.send(embed = embed)
 
-    @help.command(name = 'fun')
-    async def fun_subcommand(self, ctx):
+    @help.command(name = 'random')
+    async def random_subcommand(self, ctx):
 
         with open(r"prefixes.json", 'r') as f:
             prefixes = json.load(f)
         
         x = prefixes[str(ctx.guild.id)] if str(ctx.guild.id) in prefixes else 'le '
 
-        embed = discord.Embed(title = ":smile: Fun Commands", description = '`fat`, `meme`, `automemes`, `stopmemes`, `guess`, `status`', color = random.choice(mainbot.random_colors))
+        embed = discord.Embed(title = ":smile: Random Commands", description = '`fat`, `guess`, `status`', color = random.choice(mainbot.random_colors))
         embed.set_footer(text = f'use `{x}` before each command!')
         await ctx.send(embed = embed)
 
@@ -73,19 +69,6 @@ class help(commands.Cog):
         embed = discord.Embed(title = ":hammer: Moderation Commands", description = '`kick`, `ban`, `unban`, `purge`, `userinfo`, `warn`, `createrole`, `changenick`, `reactionmessage`, `membermessage`, `setmembermessage`, `setchannelid`, `viewmembermessage`, `autopurge`, `stoppurge`, `new`, `delete`, `lock`, `unlock`', color = random.choice(mainbot.random_colors))
         embed.set_footer(text = f'use `{x}` before each command!')
         await ctx.send(embed = embed)
-
-    @help.command(name = 'other')
-    async def other_subcommand(self, ctx):
-
-        with open(r"prefixes.json", 'r') as f:
-            prefixes = json.load(f)
-        
-        x = prefixes[str(ctx.guild.id)] if str(ctx.guild.id) in prefixes else 'le '
-
-        embed = discord.Embed(title = ':alien: Other Commands', description = '`mainstuff`, `dmdev`', color = random.choice(mainbot.random_colors))
-        embed.set_footer(text = f'use `{x}` before each command!')
-        await ctx.send(embed = embed)
-
     
     @help.command(name = 'settings')
     async def settings_subcommand(self, ctx):
@@ -98,20 +81,6 @@ class help(commands.Cog):
         embed = discord.Embed(title = ':axe: Setting Commands', description = '`setprefix`, `resetprefix`', color = random.choice(mainbot.random_colors))
         embed.set_footer(text = f'use `{x}` before each command!')
         await ctx.send(embed = embed)
-
-
-    @help.command(name = 'text')
-    async def text_subcommand(self, ctx):
-
-        with open(r"prefixes.json", 'r') as f:
-            prefixes = json.load(f)
-        
-        x = prefixes[str(ctx.guild.id)] if str(ctx.guild.id) in prefixes else 'le '
-
-        embed = discord.Embed(title = ':b: Text Commands', description = '`bify`, `echo`, `ubi`, `underline`, `bold`, `italics`, `codeblock`, `strikethrough`, `underlineitalics`, `underlinebold`, `bolditalics`', color = random.choice(mainbot.random_colors))
-        embed.set_footer(text = f'use `{x}` before each command!')
-        await ctx.send(embed = embed)
-
 
 async def setup(bot):
     await bot.add_cog(help(bot)) 
