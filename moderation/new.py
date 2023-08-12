1072fcc655a8cf328e41d3da76fe54f90894e98d
rushil <64613767+rushil534@users.noreply.github.com>
2023-08-11 21:20:40 -0700

    Update new.py

--- moderation/new.py
+++ moderation/new.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import discord
 from discord.ext.commands import has_permissions
+from discord import ChannelType
 
 class new(commands.Cog):
     def __init__(self, bot):
@@ -9,7 +10,7 @@ class new(commands.Cog):
     @commands.group()
     async def new(self, ctx):
         if ctx.subcommand_passed is None:
-            await ctx.send('Proper Usage: `new [category/channel/role]`')
+            await ctx.send('Proper Usage: `new [category/channel/role/thread]`')
         elif ctx.invoked_subcommand is None:
             await ctx.send('not an option')
     
@@ -59,6 +60,36 @@ class new(commands.Cog):
         except Exception:
             await ctx.send('you made an error somewhere')
 
+    @new.command(name = 'thread')
+    @commands.guild_only()
+    @has_permissions(create_public_threads = True, create_private_threads = True)
+    async def new_thread_subcommand(self, ctx, channel: discord.TextChannel = None, *, name: str = None):
+        if channel == None:
+            await ctx.send('i need a channel to create a thread in')
+            return
+        
+        if name == None:
+            await ctx.send('i need a name for this thread')
+            return
+        
+        await ctx.send('do you want a `private` or a `public` thread?')
+        msg = await self.bot.wait_for('message', check = lambda m: m.author == ctx.author)
+
+        if msg.content.lower() == 'public':
+            try:
+                await channel.create_thread(name = name, type = ChannelType.public_thread)
+                await ctx.send(f'Thread `{name}` Created!')
+            except Exception:
+                await ctx.send('you made an error somewhere')
+        elif msg.content.lower() == 'private':
+            try:
+                await channel.create_thread(name = name, type = ChannelType.private_thread)
+                await ctx.send(f'Thread `{name}` Created!')
+            except Exception:
+                await ctx.send('you made an error somewhere')
+        else:
+            await ctx.send('that\'s not an option')
+
     @new_category_subcommand.error
     async def new_category_subcommand_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
@@ -75,6 +106,14 @@ class new(commands.Cog):
     async def new_role_subcommand_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             await ctx.send('You don\'t have the perm: `Manage Roles`!')
+
+    @new_thread_subcommand.error
+    async def new_thread_subcommand_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send('You don\'t have either the `Create Public Threads` or `Create Private Threads` Perm!')
+        if isinstance(error, commands.BadArgument):
+            await ctx.send('that channel is invalid')
+        
 
 async def setup(bot):
     await bot.add_cog(new(bot))
